06323ee53f040e85975d6aa9f1a2b41af9c2d61a
SHUBHI NIGAM <54998601+ShubhiNigam29@users.noreply.github.com>
2021-02-07 13:16:15 -0500

    Updated time on the circle and text

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = () => {
 
     // Time display
     const timeDisplay = document.querySelector('.time-display');
+    const timeSelect = document.querySelectorAll('.time-select button');
 
     // Get the length of the outline
     const outlineLength = outline.getTotalLength();
@@ -23,6 +24,14 @@ const app = () => {
         checkPlaying(song);
     });
 
+    // Select duration
+    timeSelect.forEach(option => {
+        option.addEventListener('click', function() {
+            fakeDuration = this.getAttribute('data-time');
+            timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`;
+        })
+    })
+
     // Function to start and stop the song
     const checkPlaying = song => {
         if(song.paused) {
@@ -33,6 +42,28 @@ const app = () => {
             song.pause();
             video.pause();
             play.src = './svg/play.svg';
+        }
+    };
+
+    // Animate the circle for time display
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+
+        // Animate the circle
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDasharray = progress;
+
+        // Animate the time text
+        timeDisplay.textContent = `${minutes}:${seconds}`;
+
+        if(currentTime >= fakeDuration) {
+            song.pause();
+            song.currentTime = 0;
+            play.src = './svg/play.svg';
+            video.pause();
         }
     }
 }
